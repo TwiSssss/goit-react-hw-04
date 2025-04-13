a123892a91d8c31ac540703820133bc81172c085
Vlad Tsokota <v.tsokota.69@gmail.com>
2025-04-13 14:53:46 +0300

    push1

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,58 @@
-import { useEffect, useState } from "react";
-import ContactForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
-import ContactList from "./components/ContactList/ContactList";
-import userData from "./data/userData.json";
+import { useState, useEffect } from 'react';
+import { fetchImages } from './api';
+import SearchBar from './components/SearchBar/SearchBar';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import ImageModal from './components/ImageModal/ImageModal';
+import { Toaster } from 'react-hot-toast';
 
-export const App = () => {
-    const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem("userData")) ?? userData);
-    const [filter, setFilter] = useState("");
-    useEffect(() => {
-        localStorage.setItem("userData", JSON.stringify(contacts));
-    }, [contacts]);
-    const handleFilterChange = (event) => {
-        setFilter(event.target.value);
+const App = () => {
+  const [value, setValue] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [loadMore, setLoadMore] = useState(false);
+
+  useEffect(() => {
+    if (!value) return;
+      const loadImages = async () => {
+      setLoading(true);
+      setError(null);
+      
+      try {
+        const data = await fetchImages(value, page);
+        setImages((prev) => (page === 1 ? data.results : [...prev, ...data.results]));
+        setLoadMore(data.results.length > 0);
+      } catch (err) {
+        setError('Error loading images');
+      } finally {
+        setLoading(false);
+      }
     };
-    const filterContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()));
-    const handleDelete = (id) => {
-        const rmwContacts = contacts.filter((contact) => contact.id !== id);
-        setContacts(rmwContacts);
-    };
-    // localStorage.removeItem('userData');
-    return (
-        <div>
-            <h1>Phonebook</h1>
-            <ContactForm contacts={contacts} setContacts={setContacts} />
-            <SearchBox value={filter} onChange={handleFilterChange} />
-            <ContactList contacts={filterContacts} onDelete={handleDelete} />
-        </div>
-    );
+    loadImages();
+  }, [value, page]);
+
+  const handleSearch = (newValue) => {
+    setValue(newValue);
+    setPage(1);
+    setImages([]);
+  };
+  const loadEvenMore = () => setPage((prev) => prev + 1);
+  return (
+    <div>
+      <Toaster />
+      <SearchBar onSubmit={handleSearch} />
+      {error && <ErrorMessage message={error} />}
+      <ImageGallery images={images} openModal={setSelectedImage} />
+      {loading && <Loader />}
+      {loadMore && !loading && <LoadMoreBtn onClick={loadEvenMore} />}
+      <ImageModal image={selectedImage} isOpen={!!selectedImage} onClose={() => setSelectedImage(null)} />
+    </div>
+  );
 };
+
+export default App;
